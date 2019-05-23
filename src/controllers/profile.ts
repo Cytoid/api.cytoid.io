521cb76c23ecb208087ad09ab56c1891a6f7dfbf
Neo <me@neoto.xin>
2019-05-24 00:54:31 +0800

    User Exp

--- src/controllers/profile.ts
+++ src/controllers/profile.ts
@@ -1,5 +1,6 @@
-import {Get, JsonController, Param} from 'routing-controllers'
+import {Get, JsonController, NotFoundError, Param} from 'routing-controllers'
 import {getRepository} from 'typeorm'
+import {level} from 'winston'
 import Profile from '../models/profile'
 import User from '../models/user'
 import BaseController from './base'
@@ -15,6 +16,9 @@ export default class ProfileController extends BaseController {
     const user = await this.userRepo.findOne({
       where: isUUID ? {id} : {uid: id},
     })
+    if (!user) {
+      throw new NotFoundError()
+    }
     const profile = await this.profileRepo.findOne({
       where: {id: user.id},
     })
@@ -25,6 +29,7 @@ export default class ProfileController extends BaseController {
       rating: await this.personalRating(user.id),
       grade: await this.gradeDistribution(user.id),
       activities: await this.userActivity(user.id),
+      exp: await this.exp(user.id)
     }
   }
 
@@ -54,17 +59,21 @@ group by grade;`, [uuid])
   }
 
   public userActivity(uuid: string) {
-    const activities = this.db.createQueryBuilder()
+    const activities: any = this.db.createQueryBuilder()
       .select([
         'count(records) as total_ranked_plays',
+        'sum(chart."notesCount") as cleared_notes',
         "max((records.details -> 'maxCombo')::integer) as max_combo",
         'avg(records.accuracy) as average_ranked_accuracy',
         'sum(records.score) as total_ranked_score',
       ])
       .from('records', 'records')
+      .innerJoin('charts', 'chart', 'records."chartId" = chart.id')
       .where('records."ownerId"=:uuid', { uuid })
       .execute()
-    return activities
+    activities.total_ranked_plays = parseInt(activities.total_ranked_plays, 10)
+    activities.cleared_notes = parseInt(activities.cleared_notes, 10)
+    activities.total_ranked_plays = parseInt(activities.total_ranked_plays, 10)
   }
 
   public personalRating(uuid: string) {
@@ -82,5 +91,42 @@ FROM records as r
 JOIN charts c ON r."chartId" = c.id
 WHERE r."ownerId"=$1) as v;`, [uuid])
       .then((result) => result[0].rating)
+  }
+
+  public exp(uuid: string) {
+    return this.db.query(`
+WITH scores AS (
+ SELECT charts."notesCount" * (charts.difficulty / 15) +
+  levels.duration / 60 * 100 * (CASE WHEN records.ranked THEN 1 ELSE 0.5 END) AS base,
+  records.score as score,
+  levels.id as level
+ FROM records
+ JOIN charts on records."chartId" = charts.id
+ JOIN levels on charts."levelId" = levels.id
+ WHERE records."ownerId" = $1
+  AND charts.difficulty BETWEEN 1 AND 16
+),
+chart_scores AS (
+ SELECT max(pow(scores.score / 1000000, 2) * (scores.base * 1.5)) as score
+ FROM scores
+ GROUP BY scores.level
+)
+SELECT sum(sqrt(scores.score / 1000000) * scores.base) as basic_exp,
+       sum(chart_scores.score) as level_exp
+FROM scores, chart_scores;`, [uuid])
+      .then((result) => {
+        const basicExp = result[0].basic_exp
+        const levelExp = result[0].level_exp
+        const totalExp = basicExp + levelExp
+        const currentLevel = 1 / 30 * (Math.sqrt(6 * totalExp + 400) + 20) + 1
+        const nextLevelExp = 150 * (currentLevel * currentLevel) - 200 * currentLevel
+        return {
+          basicExp,
+          levelExp,
+          totalExp,
+          currentLevel,
+          nextLevelExp,
+        }
+      })
   }
 }
