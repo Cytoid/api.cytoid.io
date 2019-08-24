925b63fa33cb16492d4a6893a0aeb941f47ec43c
Zhixing Zhang <me@neoto.xin>
2019-08-23 22:23:16 -0400

    cookie bugfix

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ app.use(session({
   signed: true, /** (boolean) signed or not (default true) */
   rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
   renew: true, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
-  domain: conf.host + ':' + conf.port,
+  domain: (new URL(conf.webURL)).hostname.split('.').slice(-2).join('.'),
 }, app))
 
 app.use(passport.initialize())
