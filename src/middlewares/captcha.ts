abead881309ca5a606111094e255467819eca5ba
Zhixing Zhang <me@neoto.xin>
2019-10-22 23:59:57 -0400

    Temp Disable Captcha

--- src/middlewares/captcha.ts
+++ src/middlewares/captcha.ts
@@ -5,6 +5,7 @@ import getIP from '../utils/ip'
 
 export default function createCaptchaValidator(action: string) {
   function use(context: Context, next: (err?: any) => Promise<any>): Promise<any> {
+    return next()
     const token = context.request.body.token
     if (token === '6gH2cFOhN&R2qZGoHP6@I*zhlGntjrN1k4aZ3XS#TUj7K^cG$v') {
       return next()
